87ab0be92db7085dd56e27abe3e62a16a2e1ba4c
Danish <dkhans2001@gmail.com>
2025-02-14 06:19:23 -0600

    fetch

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json()) // Convert the response to JSON
+  .then((json) => renderBooks(json)); // Pass the JSON data to renderBooks
+
 }
 
 function renderBooks(books) {
